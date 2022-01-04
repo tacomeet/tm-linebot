900e0ffadd0c49cbeaaa95184a5e72fa7c934e6b
moromin <34294957+moromin@users.noreply.github.com>
2022-01-04 19:18:58 +0900

    Add: models

---
+++ models/models.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from database.database import db
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.String(), primary_key=True)
+    name = db.Column(db.String())
+    status = db.Column(db.Integer())
+    thread_ts = db.Column(db.String())
+    follow = db.Column(db.Boolean())
+    created_at = db.Column(db.DateTime(), default=datetime.now)
