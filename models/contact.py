bf901a3380c450aa98a55935ad36d394cba09ee6
moromin <34294957+moromin@users.noreply.github.com>
2022-01-05 17:55:56 +0900

    Update: contact to create table

--- models/contact.py
+++ models/contact.py
@@ -1,4 +1,7 @@
-class Contact:
+from database.database import db
+
+
+class Contact(db.Model):
 	"""
 	Contact class
 
@@ -6,22 +9,28 @@ class Contact:
 		_thread_map (dict): map of user ID to slack contact thread
 	"""
 
-	def __init__(self) -> None:
-		self._thread_map = dict()
-		self._user_id_map = dict()
+	__tablename__ = 'contacts'
 
-	def register(self, user_id, thread_ts):
-		self._set_thread(user_id, thread_ts)
-		self._set_user(user_id, thread_ts)
+	ts = db.Column(db.String(), primary_key=True)
+	user_id = db.Column(db.String())
 
-	def _set_thread(self, user_id, thread_ts):
-		self._thread_map[user_id] = thread_ts
 
-	def _set_user(self, user_id, thread_ts):
-		self._user_id_map[thread_ts] = user_id
+	# def __init__(self) -> None:
+	# 	self._thread_map = dict()
+	# 	self._user_id_map = dict()
 
-	def get_thread(self, user_id):
-		return self._thread_map.get(user_id)
+	# def register(self, user_id, thread_ts):
+	# 	self._set_thread(user_id, thread_ts)
+	# 	self._set_user(user_id, thread_ts)
 
-	def get_user(self, thread_ts):
-		return self._user_id_map.get(thread_ts)
+	# def _set_thread(self, user_id, thread_ts):
+	# 	self._thread_map[user_id] = thread_ts
+
+	# def _set_user(self, user_id, thread_ts):
+	# 	self._user_id_map[thread_ts] = user_id
+
+	# def get_thread(self, user_id):
+	# 	return self._thread_map.get(user_id)
+
+	# def get_user(self, thread_ts):
+	# 	return self._user_id_map.get(thread_ts)
