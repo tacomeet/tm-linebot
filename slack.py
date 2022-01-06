651a0d37119784a49bb14d0c81f180929304d30b
takumihara <takumihara1226@gmail.com>
2022-01-06 19:07:36 +0900

    update: separate slack channel

--- slack.py
+++ slack.py
@@ -4,17 +4,18 @@ import config
 
 
 # Load channel from config?
-channel = '#api'
+CHANNEL_CONTACT = '#line-contact'
+CHANNEL_OTHER = '#line-other'
 token = config.SLACK_TOKEN
 client = WebClient(token=token)
 
 
-def send_message(msg, ts=None):
+def send_message(msg, ts=None, channel=CHANNEL_OTHER):
     return client.chat_postMessage(channel=channel, text=msg, thread_ts=ts)
 
 
 def start_contact(name):
-    return send_message(name + 'さんからお問い合わせがありました！')
+    return send_message(name + 'さんからお問い合わせがありました！', CHANNEL_CONTACT)
 
 
 def send_msg_to_thread(name, content, ts):
