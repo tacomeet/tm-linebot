b90304a7ff9449b1b28aa4b601d37f784cde93cb
takumihara <takumihara1226@gmail.com>
2022-01-06 21:13:31 +0900

    fix: set channel for msging thread

--- slack.py
+++ slack.py
@@ -18,7 +18,7 @@ def start_contact(name):
 
 
 def send_msg_to_thread(name, content, ts):
-    send_message(name + 'さんからのお問い合わせ内容：\n' + content, ts)
+    send_message(name + 'さんからのお問い合わせ内容：\n' + content, ts, channel=CHANNEL_CONTACT)
 
 
 def follow(name):
