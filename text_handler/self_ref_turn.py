6fcaeac02467dc5692ef60468d6bd5adac10e811
takumihara <takumihara1226@gmail.com>
2022-01-12 18:46:44 +0900

    add: text handler for turning point

---
+++ text_handler/self_ref_turn.py
@@ -0,0 +1,34 @@
+from linebot.models import TextSendMessage
+
+import message as ms
+import line
+
+
+def self_ref_exp(line_bot_api, user, event):
+    text = event.message.text
+    if text == ms.default.KEY_NEXT:
+        msg = _route_next(user)
+        if msg:
+            line.reply_msg(line_bot_api, event, msg)
+            # line_bot_api.push_message(user.get_id(), TextSendMessage(text=ms.default.ASK_FOR_NEXT))
+
+
+def _route_next(user):
+    ss_stage = user.get_session_stage()
+    msg = None
+    if ss_stage == 2:
+        msg = ms.self_ref.TURN_2
+    if ss_stage == 3:
+        msg = ms.self_ref.TURN_3
+    elif ss_stage == 4:
+        msg = ms.self_ref.TURN_4
+    elif ss_stage == 5:
+        msg = ms.self_ref.TURN_5
+    elif ss_stage == 6:
+        msg = ms.self_ref.TURN_6
+    if msg:
+        user.increment_session_stage()
+        return msg
+    if ss_stage == 7:
+        user.reset()
+        return ms.default.END
